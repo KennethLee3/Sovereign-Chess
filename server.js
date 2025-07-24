986919aa56afb9e595cf7462b80a30d44237f3f3
KennethLee3 <kelee.3rd@gmail.com>
2025-07-24 13:09:32 -0400

    rook movement implemented

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const colorString = "wksgoxpnyzru";
 const pieceString = "KQRBNP";
 const SIZE = 16;
+const sColor = [
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', 'red', ' ', ' ', ' ', ' ', ' ', ' ', 'blue', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', 'yellow', ' ', 'purple', 'brown', ' ', 'green', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', 'silver', ' ', ' ', 'gold', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', 'orange', ' ', 'white', 'black', ' ', 'royalblue', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', 'royalblue', ' ', 'black', 'white', ' ', 'orange', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', 'gold', ' ', ' ', 'silver', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', 'green', ' ', 'brown', 'purple', ' ', 'yellow', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', 'blue', ' ', ' ', ' ', ' ', ' ', ' ', 'red', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+];
 
 const express = require("express");
 const app = express();
@@ -17,11 +35,7 @@ app.post("/move", (req, res) => {
   
   console.log(`Old board position: ${board}`);
   console.log(`Player moved ${from} to ${to}`);
-
-  if (checkMove(board, from, to)) {
-    board = conductMove(board, from, to);
-  }
-
+  board = conductMove(board, from, to);
   console.log(`New board position: ${board}`);
 
   res.json({
@@ -37,9 +51,6 @@ const server = app.listen(port, () =>
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
-function checkMove(board, from, to) {
-  return true;
-}
 function conductMove(inputString, from, to) {
   let board = Array.from({ length: SIZE }, () => Array(SIZE).fill(""));
   let color = Array.from({ length: SIZE }, () => Array(SIZE).fill(""));
@@ -88,14 +99,21 @@ function conductMove(inputString, from, to) {
   let toC = Number(to.charAt(0).charCodeAt()) - 65;
   let toR = 16 - Number(to.substring(1));
 
-  board[toR][toC] = board[fromR][fromC];
-  color[toR][toC] = color[fromR][fromC];
-  board[fromR][fromC] = "";
-  color[fromR][fromC] = "";
+  if (checkMove(board, color, fromC, fromR, toC, toR)) {
+    turn = turn === "w" ? "k" : "w";
 
-  // Promote pawns
-  if (board[toR][toC] == "P" && toR >= 6 && toR < 10 && toC >= 6 && toC < 10) {
-    board[toR][toC] = "Q";
+    board[toR][toC] = board[fromR][fromC];
+    color[toR][toC] = color[fromR][fromC];
+    board[fromR][fromC] = "";
+    color[fromR][fromC] = "";
+
+    // Promote pawns
+    if (board[toR][toC] == "P" && toR >= 6 && toR < 10 && toC >= 6 && toC < 10) {
+      board[toR][toC] = "Q";
+    }
+  }
+  else {
+    console.log("Move is illegal.");
   }
   
   let string = "";
@@ -127,6 +145,141 @@ function conductMove(inputString, from, to) {
     }
     string += "/";
   }
-  string += turn === "w" ? "k" : "w";
+  string += turn;
   return string;
+}
+function checkMove(board, color, fromC, fromR, toC, toR) {
+  //if (!isAvailableSquare(board, color, fromC, fromR, toC, toR)) {
+  //  return false;
+  //}
+
+  switch (board[fromR][fromC]) {
+    case "K":
+      return checkKingMovement(board, fromC, fromR, toC, toR);
+    case "Q":
+      return checkRookMovement(board, fromC, fromR, toC, toR) || checkBishopMovement(board, fromC, fromR, toC, toR);
+    case "R":
+      return checkRookMovement(board, fromC, fromR, toC, toR);
+    case "B":
+      return checkBishopMovement(board, fromC, fromR, toC, toR);
+    case "N":
+      return checkKnightMovement(board, fromC, fromR, toC, toR);
+    case "P":
+      return checkPawnMovement(board, fromC, fromR, toC, toR);
+  }
+
+  return false;
+}
+function checkKingMovement(board, fromC, fromR, toC, toR) {
+  if (Math.abs(fromC - toC) > 1) {
+    return false;
+  }
+  if (Math.abs(fromR - toR) > 1) {
+    return false;
+  }
+  // TODO: add castling
+  return true;
+}
+function checkRookMovement(board, fromC, fromR, toC, toR) {
+  // Same column
+  if (fromC == toC) {
+    // Check if moving too far
+    if (Math.abs(toR - fromR) > 8) {
+      return false;
+    }
+    // Check if path is empty
+    let invertVar = (toR - fromR) / Math.abs(toR - fromR); // Positive one if positive move
+    for (let r = fromR + invertVar; invertVar === 1 ? (r < toR) : (r > toR); r += invertVar) {
+      if (board[r][fromC] != "") {
+        return false;
+      }
+    }
+    return true;
+  }
+  // Same row
+  if (fromR == toR) {
+    // Check if moving too far
+    if (Math.abs(toC - fromC) > 8) {
+      return false;
+    }
+    // Check if path is empty
+    let invertVar = (toC - fromC) / Math.abs(toC - fromC); // Positive one if positive move
+    for (let c = fromC + invertVar; invertVar === 1 ? (c < toC) : (c > toC); c += invertVar) {
+      if (board[fromR][c] != "") {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+}
+function checkBishopMovement(board, fromC, fromR, toC, toR) {
+  return false;
+}
+function checkKnightMovement(board, fromC, fromR, toC, toR) {
+  return false;
+}
+function checkPawnMovement(board, fromC, fromR, toC, toR) {
+  return false;
+}
+function returnControlColors(color, pieceColor) {
+  let controlColors = [];
+  for (let r = 4; r < SIZE - 4; r++) {
+    for (let c = 4; c < SIZE - 4; c++) {
+      // Check if this square has a piece of the given color.
+      if (color[r][c] == pieceColor) {
+        switch (sColor[r][c]) {
+          case " ":
+          case "white":
+            // Don't allow opponent to control opponent's color.
+          case "black":
+            // Don't allow opponent to control opponent's color.
+            break;
+          case "silver":
+            controlColors.push("s");
+            break;
+          case "gold":
+            controlColors.push("g");
+            break;
+          case "purple":
+            controlColors.push("p");
+            break;
+          case "brown":
+            controlColors.push("n");
+            break;
+          case "royalblue":
+            controlColors.push("x");
+            break;
+          case "orange":
+            controlColors.push("o");
+            break;
+          case "yellow":
+            controlColors.push("y");
+            break;
+          case "green":
+            controlColors.push("z");
+            break;
+          case "red":
+            controlColors.push("r");
+            break;
+          case "blue":
+            controlColors.push("u");
+            break;
+        }
+      }
+    }
+  }
+  return controlColors;
+}
+function isAvailableSquare(board, color, fromC, fromR, toC, toR) {
+  let colorControl = [];
+  colorControl.push(color[fromR][fromC]);
+  while(colorControl.length > 0) {
+    let nextColor = colorControl.pop();
+    if (color[toR][toC] == nextColor) {
+      return false;
+    }
+    colorControl.push(returnControlColors(color, nextColor));
+  }
+  return true;
 }
